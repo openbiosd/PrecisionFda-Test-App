57a9e639b8d80e53b9052c37c1311cfdb79bbaa3
Vladimir Gainullin <Vlad@Vladimirs-MacBook-Pro.local>
2016-10-09 09:48:49 -0700

    cut out unused function for this project

--- exac_parser.py
+++ exac_parser.py
@@ -1,7 +1,7 @@
 '''
 Missense variant miner:
-find missense variants in ExAC, score them using PROVEAN, MutPred and dbNSFP
-return csv file.
+find missense variants in ExAC
+returns csv file.
 
 
 
@@ -61,201 +61,6 @@ Chr = "17"
 os.chdir("../Data/")
 cwd = os.getcwd()
 
-
-# df = pd.read_csv('foo.csv', index_col=0)
-def findPROVEANscores(ENSP):
-    # read from tsv.gz file
-    with gzip.open('PROVEAN_scores_ensembl66_human.tsv.gz', 'rt') as tsvin, open(FILENAME1, 'wt') as csvout:
-        csvout = csv.writer(csvout)
-        tsvin = csv.reader(tsvin, delimiter='\t', quoting=csv.QUOTE_NONE)
-        for i in range(1):
-            row1 = next(tsvin)
-            print(row1)
-            csvout.writerows([row1])
-            i = +1
-        for row in tsvin:
-            count = row[0]
-            # print (count, ENSP)
-            if count == ENSP:
-                csvout.writerows([row[0:23]])
-
-
-def formatPROVEAN(input):
-    df = (pd.read_csv(input))
-    short_df = df.drop(df.columns[[0, 1]], axis=1)
-    short_df['AA'] = (short_df.T.idxmax())
-    short_df['position'] = df['position'].apply(str)
-    short_df['Mutation'] = short_df['AA'].astype(str) + short_df['position'].astype(str)
-    df.to_csv(FILENAME, sep='\t')
-    print(short_df)
-
-
-def dataframeExAC(SYMBOL):
-    print("Open")
-
-    # read from tsv.gz file
-    with gzip.open('ExAC.r0.3.1.sites.vep.vcf.gz', 'rt') as tsvin, open(FILENAME2, 'wt') as csvout:
-        print("open")
-        comments = count_comments(tsvin)
-        result = OrderedDict()
-        csvout = csv.writer(csvout)
-        tsvin = pd.read_table(tsvin, skiprows=comments, names=VCF_HEADER,
-                              usecols=range(8))  # delimiter='|',quoting=csv.QUOTE_NONE, skiprows = comments)
-        print('looking for this query: ', SYMBOL)
-
-        for i, line in enumerate(lines(tsvin)):
-            for key in line.keys():
-                if key not in result:
-                    result[key] = [None] * i
-
-            for key in result.keys():
-                result[key].append(line.get(key, None))
-
-        return pd.DataFrame(result)
-
-
-def generatorExAC(filename, Chr):
-    print('searching Chrom:', Chr, 'in file: ', filename)
-    lines(filename, Chr)
-
-
-# for i in range (1):
-#     row1 = next(tsvin)
-#     print (row1)
-#     print ('found', len(row1), 'rows')
-#     csvout.writerows([row1])
-#     i+=1
-# for line in tsvin:
-#
-#     if line.startswith('#'):
-#         continue
-#     else:
-#         parse(line)
-
-def mineMutPred(ENST, UniProt, Chr):
-    ChrMutPredFilesDict = {
-        '1': 'dbnsfp_chr1_mutpred.txt',
-        '2': 'dbnsfp_chr2_mutpred.txt',
-        '3': 'dbnsfp_chr3_mutpred.txt',
-        '4': 'dbnsfp_chr4_mutpred.txt',  # ENSG is row 19"
-        '5': 'dbnsfp_chr5_mutpred.txt',
-        '6': 'dbnsfp_chr6_mutpred.txt',
-        '7': 'dbnsfp_chr7_mutpred.txt',
-        '8': 'dbnsfp_chr8_mutpred.txt',
-        '9': 'dbnsfp_chr9_mutpred.txt',
-        '10': 'dbnsfp_chr10_mutpred.txt',
-        '11': 'dbnsfp_chr11_mutpred.txt',
-        '12': 'dbnsfp_chr12_mutpred.txt',
-        '13': 'dbnsfp_chr13_mutpred.txt',
-        '14': 'dbnsfp_chr14_mutpred.txt',
-        '15': 'dbnsfp_chr15_mutpred.txt',
-        '16': 'dbnsfp_chr16_mutpred.txt',
-        '17': 'dbnsfp_chr17_mutpred.txt',
-        '18': 'dbnsfp_chr18_mutpred.txt',
-        '19': 'dbnsfp_chr19_mutpred.txt',
-        '20': 'dbnsfp_chr20_mutpred.txt',
-        '21': 'dbnsfp_chr21_mutpred.txt',
-        'X': 'dbnsfp_chrX_mutpred.txt',
-        'Y': 'dbnsfp_chrY_mutpred.txt',
-    }
-
-    # TODO: This is working but is very slow, need to make into Generator?
-    # read from tsv.gz file
-    with tarfile.open('Mutpred.tar.gz') as tsvin, open(FILENAME3, 'wt') as csvout:
-        a = csv.writer(csvout)
-        first_row = ('Genomic_location', 'ProteinID', 'MUTATION', 'MUTPRED Score', 'Hypothesis')
-        a.writerows([first_row])
-        tar = tsvin.extractfile(ChrMutPredFilesDict[Chr])
-        print(tar)
-
-        tp = pd.read_table(tar, delimiter='\t', quoting=csv.QUOTE_NONE)
-        print(type(tp))
-
-        # tsvin = csv.reader(tsvin, delimiter='\t',quoting=csv.QUOTE_NONE)
-        print('looking for this query: ', ENST, UniProt, 'in chromosome',Chr)
-        # TODO: write first row to file
-        # for i in range(1):
-        #     row1 = next(tp)
-        #     print(row1)
-        #     print('found ', len(row1), 'rows' )
-        #     csvout.writerows([row1])
-        #     i=+1
-        variants = 0
-        for index, row in tp.iterrows():
-
-            count = row[1]
-            # print(count,ENST, UniProt)
-            if count == ENST or count == UniProt:
-                variants += 1
-                print(variants, ' writing variant scores ', row[0:2])
-                a.writerows([row[0:len(row)]])
-
-        print('found ', variants, 'variant MutPred Scores')
-
-
-def mine_dbNSFP(Chr, ENSG):
-    # TODO: should be able to read file names and iterate through them
-    chrfilesdict = {
-        'chr1': 'dbNSFP3.2c_variant.chr1',
-        'chr2': 'dbNSFP3.2c_variant.chr2',
-        'chr3': 'dbNSFP3.2c_variant.chr3',
-        'chr4': 'dbNSFP3.2c_variant.chr4',
-        'chr5': 'dbNSFP3.2c_variant.chr5',
-        'chr6': 'dbNSFP3.2c_variant.chr6',
-        'chr7': 'dbNSFP3.2c_variant.chr7',
-        'chr8': 'dbNSFP3.2c_variant.chr8',
-        'chr9': 'dbNSFP3.2c_variant.chr9',
-        'chr10': 'dbNSFP3.2c_variant.chr10',
-        'chr11': 'dbNSFP3.2c_variant.chr11',
-        'chr12': 'dbNSFP3.2c_variant.chr12',
-        'chr13': 'dbNSFP3.2c_variant.chr13',
-        'chr14': 'dbNSFP3.2c_variant.chr14',
-        'chr15': 'dbNSFP3.2c_variant.chr15',
-        'chr16': 'dbNSFP3.2c_variant.chr16',
-        'chr17': 'dbNSFP3.2c_variant.chr17',
-        'chr18': 'dbNSFP3.2c_variant.chr18',
-        'chr19': 'dbNSFP3.2c_variant.chr19',
-        'chr20': 'dbNSFP3.2c_variant.chr20',
-        'chr21': 'dbNSFP3.2c_variant.chr21',
-        'chrX': 'dbNSFP3.2c_variant.chrX',
-        'chrM': 'dbNSFP3.2c_variant.chrM',
-    }
-    # read from tsv.gz file
-    with zipfile.ZipFile('dbNSFPv3.2c.zip', 'r') as tsvin, open(FILENAME4, 'wt') as csvout:
-        tp = pd.read_csv(tsvin.open(chrfilesdict[Chr]), delimiter='\t', quoting=csv.QUOTE_NONE, iterator=True,
-                         dtype=object, chunksize=40)  # header = None)
-        writer = csv.writer(csvout)
-        for i in range(1):
-            row1 = next(tp)
-            print(row1)
-            print('found ', len(row1), 'rows')
-            writer.writerows([row1[1:len(row1)]])
-            i = +1
-        print(chrfilesdict[Chr])
-
-        variants = 0
-        for chunk in tp:
-            row = next(chunk.itertuples())
-            count = row[20]
-            # print (row[1:len(row)])
-            if count == ENSG:
-                variants += 1
-                print(variants, ' writing', ENSG, 'variant scores ', row[0])
-                writer.writerows([row[1:len(row)]])
-        print(variants)
-
-
-def extract_dbNSFP(file):
-    with open(file, 'rt') as tsvin, open(FILENAME5, 'wt') as csvout:
-        dict = []
-        df = pd.read_csv(tsvin, delimiter='\t')
-        print(df)
-        for i in range(5):
-            row = next(df)
-            print(row)
-            dict.append(row)
-            i += 1
-        print(dict)
 
 
 def count_comments(filename):
@@ -406,8 +211,6 @@ def find_good_lines(fh):
         #     break
 
 
-
-
 def _get_value(value):
     """Interpret null values and return ``None``. Return a list if the value
 	contains a comma.
@@ -441,44 +244,15 @@ def filterExACoutput(file):
     print(df.head())
 
 
-# Print iterations progress
-def printProgress(iteration, total, prefix='', suffix='', decimals=1, barLength=100):
-    """
-	Call in a loop to create terminal progress bar
-	@params:
-		iteration   - Required  : current iteration (Int)
-		total       - Required  : total iterations (Int)
-		prefix      - Optional  : prefix string (Str)
-		suffix      - Optional  : suffix string (Str)
-		decimals    - Optional  : positive number of decimals in percent complete (Int)
-		barLength   - Optional  : character length of bar (Int)
-		http://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console
-	"""
-    formatStr = "{0:." + str(decimals) + "f}"
-    percents = formatStr.format(100 * (iteration / float(total)))
-    filledLength = int(round(barLength * iteration / float(total)))
-    bar = '█' * filledLength + '-' * (barLength - filledLength)
-    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percents, '%', suffix)),
-    if iteration == total:
-        sys.stdout.write('\n')
-    sys.stdout.flush()
-
 
 def main():
-    # findPROVEANscores(ENSP)
-    # formatPROVEAN(FILENAME)
 
-    # mineExAC(ENST, Chr)
 
     lines('ExAC.r0.3.1.sites.vep.vcf.gz', Chr)
 
 
     #filterExACoutput('Exac_parse_OUT.csv')
 
-
-   # #mineMutPred(UniProt,ENST,Chr)
-# mine_dbNSFP(Chr, ENSG)
-# extract_dbNSFP(FILENAME4)
 
 
 
