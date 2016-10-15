713aa3899473d842d0e0c878cb5600da9959160a
as2272 <as2272@Anas-MacBook-Pro.local>
2016-10-15 13:52:07 -0700

    edit exac_parser2.py to add printing of files in folder created

--- exac_parser2.py
+++ exac_parser2.py
@@ -129,7 +129,9 @@ def lines(Chr):
         a.writerows([first_row])
         print ('opened file')
         print('looking for chromosome ', Chr)
-        #items = list(range(1, 23))
+        for filename in os.listdir(os.getcwd()):
+		print(filename)
+	#items = list(range(1, 23))
         #l = len(items)
         FinalResults = OrderedDict()
 
